ad3add65f62bf4db0b78efa214ad7ac9cfca2cf5
Dwayne C. Litzenberger <dlitz@dlitz.net>
2010-09-11 18:36:30 -0400

    Remove *.ex

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the ustripe package
-#
-0 4	* * *	root	[ -x /usr/bin/ustripe_maintenance ] && /usr/bin/ustripe_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(ustripe):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="ustripe" command="/usr/bin/ustripe"
